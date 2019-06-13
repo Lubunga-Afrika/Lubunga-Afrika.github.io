37a35fb9380b4a331c0530f07f76ad5449f3bf64
Mkwala <mkwalaelocho@yahoo.com>
2019-06-12 21:04:24 -0400

    sd

---
+++ main.js
@@ -0,0 +1,13 @@
+var name = "mkwala";
+
+alert("hallo " + name);
+
+name = "opportunities";
+alert("chance" + name);
+
+function showAlert() {alert("hello");
+}
+showAlert();
+
+<h1>0</h1>
+<button>increment</button>
