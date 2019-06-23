1617922916697491df5dbc4185464b36654081a0
Mkwala <mkwalaelocho@yahoo.com>
2019-06-22 21:55:38 -0400

    push

--- main.js
+++ main.js
@@ -11,3 +11,13 @@ showAlert();
 
 <h1>0</h1>
 <button>increment</button>
+
+
+<main class="page-container">
+  <article class="arrow-example">
+    <img class="target" src="https://glover.io/refcode-class/images/target.png" />
+    <img class="arrow" src="https://glover.io/refcode-class/images/arrow.png" />
+  </article>
+</main>
+
+}
